e489cc4c0dac25eecfee68313c40969ce60489fe
leydy <leydysot.20.ja@gmail.com>
2023-11-22 00:43:27 -0500

    editar y detalle proveedor

--- src/main/java/com/grupo1/almacen/controller/ProveedorController.java
+++ src/main/java/com/grupo1/almacen/controller/ProveedorController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -23,7 +24,13 @@ public class ProveedorController {
     private ProveedorRepository proveedorRepository;
     private TipoDocumentoRepository tipoDocumentoRepository;
     private TipoPersonaRepository tipoPersonaRepository;
+    @GetMapping("/proveedores/listar")
+    public  String listarProveedor(Model model){
+        List<Proveedor> listaProveedor =proveedorRepository.findAll();
+        model.addAttribute("listaProveedor",listaProveedor);
+        return "proveedor";
 
+    }
     @GetMapping("/proveedores/nuevo")
     public String formularioProveedor(Model model){
         List<Producto> listaProductos=productoRepository.findAll();
@@ -36,7 +43,24 @@ public class ProveedorController {
     @PostMapping("/proveedores/guardar")
     public String guardarProveedor(Proveedor proveedor){
         proveedorRepository.save(proveedor);
-        return "redirect:/proveedores/nuevo";
+        return "redirect:/proveedores/listar";
+    }
+    @GetMapping("/proveedores/editar/{id}")
+    public String mostrarFormularioModificarProveedor(@PathVariable("id") Long id, Model model) {
+        Proveedor proveedor=proveedorRepository.findById(id).get();
+        model.addAttribute("proveedor",proveedor);
+        model.addAttribute("tipoDocumentos",productoRepository.findAll());
+        model.addAttribute("tipoDocumentos",tipoDocumentoRepository.findAll());
+        model.addAttribute("tipoPersonas",tipoPersonaRepository.findAll());
+        model.addAttribute("listaProductos",productoRepository.findAll());
+        return"formulario-proveedor";
+
+    }
+    @GetMapping("/proveedores/detalles/{id}")
+    public String detalleProveedor(@PathVariable("id") Long id, Model model) {
+        Proveedor proveedor=proveedorRepository.findById(id).get();
+        model.addAttribute("proveedor",proveedor);
+        return"detalle-proveedor";
     }
 
 }
